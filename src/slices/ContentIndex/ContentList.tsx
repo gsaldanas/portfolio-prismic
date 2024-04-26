c597d0f15cd8f0737a8b04a7d7e116ae953e4c2a
Gaspar Saldana Sandoval <gs@Gaspars-MacBook-Air.local>
2024-04-26 10:47:52 +0200

    ref bugfix void

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -128,7 +128,9 @@ export default function ContentList({
                 key={index}
                 className="opacity-0f list-item"
                 onMouseEnter={() => onMouseEnter(index)}
-                ref={(el) => (itemsRef.current[index] = el)}
+                ref={(el) => {
+                  itemsRef.current[index] = el;
+                }}
               >
                 <Link
                   href={urlPrefix + "/" + item.uid}
